466fa4f69b5df7e464b97940e81abc7abb0c4617
Ronald Lin <ronaldlindev@gmail.com>
2024-04-11 22:47:25 -0700

    added yolo test dataframe

--- src/create_tables.py
+++ src/create_tables.py
@@ -10,12 +10,13 @@ imgs_to_id =  {ele ['id'] : ele['file_name'] for ele in data['images']}
 annotations_by_image = {imgs : {label : 0 for label in id_to_label.values()} for imgs in imgs_to_id.keys()}
 for ele in data['annotations']:
     annotations_by_image[ele['image_id']][id_to_label[ele['category_id']]] += 1
-df = pd.DataFrame(annotations_by_image).transpose()
-labels = [id_to_label[ele] for ele in id_to_label.keys()]    
+df = pd.DataFrame(annotations_by_image).transpose().sort_index(axis = 1)
 
 df.loc[len(df.index)] = df.sum(axis = 0)
 df.rename(index = {9 : "sum"}, inplace = True)
 
+
+print(df)
 os.chdir(r'tables/')
 df.to_csv(r'eval.csv')
 
--- src/test.py
+++ src/test.py
@@ -1,21 +1,38 @@
 from ultralytics import YOLO
 import sys
 import os
+import numpy as np
+import pandas as pd
 # load model
-model = YOLO(r'models/yolov8n.pt')
+model = YOLO(r'models/yolov8x.pt')
+coco_to_mood = {'person' : 'people', 'chair' : 'chair', 'dining table' : 'table', 'laptop' : 'laptop', 'backpack' : 'bag'}
 # person, chair, dining table, laptop
 def predict(file):
-    results = model(file, classes = [1, 57, 61, 64])
-    boxes = results[0].boxes.xyxy.cpu().numpy()
-    classes = results[0].boxes.cls.cpu().numpy()
+    results = model(source = file, classes = [0, 24, 56, 61, 64], device = 'mps',)
+    # boxes = results[0].boxes.cpu().numpy()
+    labels = results[0].boxes.cls.cpu().numpy()
+    # results[0].show()
     names = results[0].names
-    for ele in zip(boxes, classes):
-            box = ele[0]
-            label = names[ele[1]]
-            print(box, label)
+    return [coco_to_mood[names[id]] for id in labels]
 
 
 
-for file in os.listdir('/'):
+os.chdir(r'mood_eval-1/test/')
+results = {}
+for file in os.listdir():
       if not file.endswith('.json'):
-            predict(file)
+            instances = predict(file)
+            counts = {}
+            for object in instances:
+                counts[object] = counts.get(object, 0) + 1
+
+            results[file] = counts
+            
+df = pd.DataFrame(results).transpose().replace(np.nan, 0)
+df['laptop'] = 0
+df['table'] = 0
+df = df.sort_index(axis = 1)
+df.loc[len(df.index)] = df.sum(axis = 0)
+df.rename(index = {9 : "sum"}, inplace = True)
+print(df)
+                
