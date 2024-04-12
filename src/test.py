5b98febe16c1ffe77164339594230ca029a0e1c9
Ronald Lin <ronaldlindev@gmail.com>
2024-04-11 21:19:41 -0700

    created eval table/Users/ronaldlin/miniconda3/envs/mood/bin/python

--- src/test.py
+++ src/test.py
@@ -1,10 +1,21 @@
 from ultralytics import YOLO
-import json
+import sys
+import os
+# load model
+model = YOLO(r'models/yolov8n.pt')
+# person, chair, dining table, laptop
+def predict(file):
+    results = model(file, classes = [1, 57, 61, 64])
+    boxes = results[0].boxes.xyxy.cpu().numpy()
+    classes = results[0].boxes.cls.cpu().numpy()
+    names = results[0].names
+    for ele in zip(boxes, classes):
+            box = ele[0]
+            label = names[ele[1]]
+            print(box, label)
 
 
 
-
-model = YOLO('models/yolov8n.pt')
-
-
-                
+for file in os.listdir('/'):
+      if not file.endswith('.json'):
+            predict(file)
